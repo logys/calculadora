158fa9f052b63482965f4c8925434c38b3975f6b
Luis Antonio Sánchez Monterde <antonio.dgr.ink@gmail.com>
2020-11-03 17:39:04 -0600

    Refactorizado notación polaca inversa

--- src/app/calculadora.module.ts
+++ src/app/calculadora.module.ts
@@ -1,15 +1,13 @@
 export class Calculadora {
 	stack_string_display: string = '';
-    	private static instance: Calculadora;
-    	private constructor() { }
-	
-    	public static getInstance(): Calculadora {
+	private static instance: Calculadora;
+	private constructor() { }
+	public static getInstance(): Calculadora {
 		if (!Calculadora.instance) {
-	    		Calculadora.instance = new Calculadora();
+			Calculadora.instance = new Calculadora();
 		}
 		return Calculadora.instance;
-    	}
-
+	}
 	public stack_string(): string{
 		return this.stack_string_display;
 	}
@@ -41,26 +39,26 @@ export class Calculadora {
 		return expresion[0];
 	}
 	private operate_two(expresion: string[], index: number): string{
-		let resultado: number=0;
+		let result: number=0;
 		switch(expresion[index -1]) {
 			case '+': {
-				resultado = Number(expresion[index]) + Number(expresion[index+1]);
+				result = Number(expresion[index]) + Number(expresion[index+1]);
 				break;
 			}
 			case '-': {
-				resultado = Number(expresion[index]) - Number(expresion[index+1]);
+				result = Number(expresion[index]) - Number(expresion[index+1]);
 				break;
 			}
 			case '*': {
-				resultado = Number(expresion[index]) * Number(expresion[index+1]);
+				result = Number(expresion[index]) * Number(expresion[index+1]);
 				break;
 			}
 			case '/': {
-				resultado = Number(expresion[index]) / Number(expresion[index+1]);
+				result = Number(expresion[index]) / Number(expresion[index+1]);
 				break;
 			}
 		}
-		return String(resultado);
+		return String(result);
 	}
 	private find_two_numbers(expresion: string[]): number{
 		for(let i = 0; ;i++){
@@ -76,10 +74,11 @@ export class Calculadora {
 		let stack_resultado: string[] = [];
 		let stack_operadores: string[] = [];
 		let expresion_length = expresion.length;
+
 		for(let i = 0; i<expresion_length; i++){
 			let elemento = expresion.pop();
 			if(this.is_simbol(elemento)){
-				while(this.is_lower_precedence(elemento, stack_operadores[stack_operadores.length -1])){
+				while(this.high_presedence_in_stack(elemento, stack_operadores)){
 					stack_resultado.push(stack_operadores.pop());
 				}
 				stack_operadores.push(elemento);
@@ -92,12 +91,16 @@ export class Calculadora {
 		stack_resultado = stack_resultado.reverse();
 		return stack_resultado;
 	}
+	private high_presedence_in_stack(elemento: string, stack: string[]): boolean{
+		return this.is_lower_precedence(elemento, stack[stack.length -1]);
+
+	}
 	private is_simbol(simbol: string): boolean{
 		return simbol == '+' || simbol == '-' || simbol == '*' || simbol == '/' ;
 	}
-	private is_lower_precedence( elemento: string, top_stack: string): boolean{
+	private is_lower_precedence( elemento: string, stack_operators: string): boolean{
 		if(elemento == '+' || elemento == '-')
-			if(top_stack == '*' || top_stack == '/')
+			if(stack_operators == '*' || stack_operators == '/')
 				return true;
 		return false;
 	}
